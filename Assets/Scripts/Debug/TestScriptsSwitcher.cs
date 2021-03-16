6a3a95725231b5e6d363600b15e34739332fd6f4
bookWorm <golosov2215>
2021-03-16 17:01:58 +0300

    add fps counter

---
+++ Assets/Scripts/Debug/TestScriptsSwitcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScriptsSwitcher : MonoBehaviour
+{
+    [SerializeField] private GameObject _debugPanel;
+    [SerializeField] private bool _isTest;
+
+    private void Awake()
+    {
+        if(_isTest == false)
+        {
+            _debugPanel.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
