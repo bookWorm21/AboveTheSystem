0b39ce47b9791dbb9f74eb952c1f1e88babf6f84
bookWorm <golosov2215>
2021-03-16 17:20:19 +0300

    add unit counter for debug

--- Assets/Scripts/Buildings/BuildingProduction.cs
+++ Assets/Scripts/Buildings/BuildingProduction.cs
@@ -6,8 +6,10 @@ public class BuildingProduction : MonoBehaviour
 {
     [SerializeField] private Transform _pointForSpawn;
 
+    public event System.Action<Unit> SpawnedUnit;
     public void ProduceUnit(UnitProfile profile)
     {
-        Instantiate(profile.Prefab, _pointForSpawn.position, _pointForSpawn.rotation);
+        Unit unit = Instantiate(profile.Prefab, _pointForSpawn.position, _pointForSpawn.rotation);
+        SpawnedUnit?.Invoke(unit);
     }
 }
---
+++ Assets/Scripts/System/UnitCounter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitCounter : MonoBehaviour
+{
+    [SerializeField] private PlaceLogic _placeLogic;
+    [SerializeField] private Text _unitCountLabel;
+
+    private int _unitCount = 0;
+
+    private void OnEnable()
+    {
+        _placeLogic.SmashedBuilding += OnPlaceNewBuild;
+    }
+
+    private void OnPlaceNewBuild(Building building)
+    {
+        if(building.TryGetComponent(out BuildingProduction production))
+        {
+            production.SpawnedUnit += OnProduceNewUnit;
+        }
+    }
+
+    private void OnProduceNewUnit(Unit unit)
+    {
+        _unitCount++;
+        _unitCountLabel.text = _unitCount.ToString();
+    }
+
+    private void OnDisable()
+    {
+        _placeLogic.SmashedBuilding -= OnPlaceNewBuild;
+    }
+}
